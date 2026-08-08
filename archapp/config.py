bfb6319d3777f443b77b577c2ee616625c4ed53e
Kaushik Malapati <kaushikm@psbuild-rhel7-02.pcdsn>
2026-08-07 23:44:44 -0700

    changing pscaa to psctlws01

--- archapp/config.py
+++ archapp/config.py
@@ -3,6 +3,6 @@ config.py defines default archive appliance server properties
 """
 import os
 
-hostname = os.environ.get("ARCHAPP_HOSTNAME", "pscaa01")
-data_port = int(os.environ.get("ARCHAPP_DATA_PORT", 17668))
-mgmt_port = int(os.environ.get("ARCHAPP_MGMT_PORT", 17665))
+hostname = os.environ.get("ARCHAPP_HOSTNAME", "psctlws01")
+data_port = int(os.environ.get("ARCHAPP_DATA_PORT", 8329))
+mgmt_port = int(os.environ.get("ARCHAPP_MGMT_PORT", 8329))
